18cee7a4ccc3b61c2e385db03eceeeb7dd26e0dd
longen0707 <1393726657@qq.com>
2019-06-27 18:30:11 +0800

    111

--- shangcheng/pages/tuikuan/tuikuan.js
+++ shangcheng/pages/tuikuan/tuikuan.js
@@ -1,161 +1,70 @@
 // pages/tuikuan/tuikuan.js
-const app = getApp()
-var that;
 Page({
-
+ 
   /**
    * 页面的初始数据
    */
   data: {
-    orderid: "",
-    images: [],
-    ContentValue:"",
-    showUpload:true
-  },
-  // 点击申请退款
-  handTui: function() {
-    that = this
-    console.log(this.data.ContentValue)
-    app.postData("GetOrderData.ashx",{
-      action:"Refund",
-      orderid: that.data.orderid,
-      reason: that.data.ContentValue,
-      pics: that.data.images
-    }).then(res=>{
-      console.log(res)
-    })
-  },
 
-  // 获取用户地址
-  address() {
-    that = this
-    app.postData("GetOrderData.ashx", {
-      action: "GetOrderDetails",
-      orderid: that.data.orderid
-    }).then(res => {
-      console.log(res)
-      that.setData({
-        address: res.Result.detailsdlist
-      })
-    })
   },
-
-  // 添加图片
-  chooseImage() {
-    that = this
-    wx.chooseImage({
-      count: 3,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success(res) {
-        console.log(res)
-        const tempFilePaths = res.tempFilePaths
-        that.setData({
-          images: that.data.images.concat(res.tempFilePaths)
-        })
-        console.log(that.data.images)
-        if (that.data.images.length==3){
-          that.setData({
-            showUpload:false
-          })
-        }
-      }
-    })
-  },
-
-  // // 删除图片
-  clearImg(e){
-    that - this
-    var nowList = [];//新数据
-    var uploaderList = this.data.images;//原数据
-    for (let i = 0; i < uploaderList.length; i++) {
-      if (i == e.currentTarget.dataset.index) {
-        continue;
-      } else {
-        nowList.push(uploaderList[i])
-      }
-    }
-    that.setData({
-      images: nowList,
-      showUpload: true
-    })
-  },
-
-  // 展示图片
-  showImg(e){
-    var that = this;
-    wx.previewImage({
-      urls: that.data.images,
-      current: that.data.images[e.currentTarget.dataset.index]
-    })
-  },
-
-  // 获取用户输入退款内容
-  content(e){
-    console.log(e.detail.value)
-    that = this
-    that.setData({
-      ContentValue: e.detail.value
+  handTui:function(){
+    wx.navigateTo({
+      url: '../tkyy/tkyy',
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-    that = this
-    that.setData({
-      orderid: options.orderid
-    })
+  onLoad: function (options) {
 
-    that.address()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
