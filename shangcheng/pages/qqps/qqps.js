aca7e764c7287af8790b7a415f35fc01cd25e94e
longen0707 <1393726657@qq.com>
2019-07-05 09:17:43 +0800

    请求配送接口

--- shangcheng/pages/qqps/qqps.js
+++ shangcheng/pages/qqps/qqps.js
@@ -1,13 +1,37 @@
 // pages/qqps/qqps.js
+var app = getApp();
+var that;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list: []
   },
-  handlPin:function(){
+  handReceiving:function(e){
+    that=this;
+    let deliverid = e.currentTarget.dataset.index;
+    app.postData("GetGoodsData.ashx", {
+      action: "ReviceDeliver",
+      deliverid: deliverid
+    }).then(res => {
+      console.log(res);
+    })
+  },
+  handDeliverList: function() {
+    that = this;
+    app.postData("GetGoodsData.ashx", {
+      action: "DeliverList",
+      userid: app.globalData.userid
+    }).then(res => {
+      that.setData({
+        list: res.Result
+      })
+      console.log(that.data.list);
+    })
+  },
+  handlPin: function() {
     wx.navigateTo({
       url: '../evaluation/evaluation'
     })
@@ -15,56 +39,57 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    that = this;
+    that.handDeliverList();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- shangcheng/pages/request/request.js
+++ shangcheng/pages/request/request.js
@@ -8,72 +8,120 @@ Page({
    */
 
   data: {
-
+    share:1
   },
-  handlQqps:function(){
-    wx:wx.navigateTo({
-      url: '../qqps/qqps'
+  jian: function (e) {
+    that = this;
+    var share = that.data.share;
+    if (share < 1) {
+      wx.showToast({
+        title: '份额不能为少于0',
+        icon: 'none',
+        duration: 2000
+      })
+      return;
+    } else {
+      share--;
+      that.setData({
+        share: share
+      })
+    }
+  },
+
+
+  // 加
+  jia: function (e) {
+    that = this;
+    var share = that.data.share;
+    if (share >=12){
+      wx.showToast({
+        title: '剩余份额不足',
+        icon: 'none',
+        duration: 2000
+      })
+      return;
+    }else{
+      share++;
+      that.setData({
+        share: share
+      })
+    }
+  },
+
+  handlQqps: function() {
+    that = this;
+    app.postData("GetGoodsData.ashx", {
+      action: "RequestDeliver",
+      userid: app.globalData.userid,
+      count: that.data.share
+    }).then(res => {
+      console.log(res);
+      if (res.Result == 1) {
+        wx.navigateTo({
+          url: '../qqps/qqps'
+        })
+      } else {
+        wx.showToast({
+          title: '申请失败',
+          icon: 'none',
+          duration: 2000
+        })
+      }
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     that = this;
-    app.postData("GetGoodsData.ashx", {
-      action: "DeliverList",
-      userid: app.globalData.userid
-    }).then(res => {
-      console.log(res);
-    })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
